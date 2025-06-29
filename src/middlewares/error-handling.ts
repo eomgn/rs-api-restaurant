9296d82d139425975381e5f9946cea4c9e42dc4b
eomgn <matheeusgn21@gmail.com>
2025-06-29 11:27:36 -0300

    instalando zod e criando verificacoa de excecoes de instancia com zoderror

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
 
 //middleware criado para lidar com exceções
 export function errorHandling(
@@ -8,10 +9,19 @@ export function errorHandling(
   response: Response,
   next: NextFunction
 ) {
-  //verifica se o erro é uma instancia de AppError
+  //verifica se o erro é uma instancia de APPERROR
   if (error instanceof AppError) {
     // a grande sacada é identificar se o erro foi lançado propositalmente
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  // -----------------------------
+
+  //verifica se o erro é uma instancia de ZODERROR
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .json({ message: "validation error", issues: error.format() });
   }
 
   // caso nao seja um erro proposital é lançado um erro especifico
