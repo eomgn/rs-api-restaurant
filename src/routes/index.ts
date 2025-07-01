b73e682c1a2d94f42404a8f7f7171788d56b945d
eomgn <matheeusgn21@gmail.com>
2025-06-30 23:19:16 -0300

    criando a rota do tipo get para listar as mesas - index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { productsRoutes } from "./products-routes";
+import { tablesRoutes } from "./tables-routes";
 
 const routes = Router();
 
 //rotas de products
 routes.use("/products", productsRoutes);
+
+// rotas de tables
+routes.use("/tables", tablesRoutes);
 
 export { routes };
